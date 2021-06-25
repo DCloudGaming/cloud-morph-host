e6689c7e7b8cfccca825d5f5fb16d6d7b242125e
hieu.le <hieu.le@bytedance.com>
2021-06-25 20:19:56 +0800

    WIP - First commit

---
+++ CloudMorphHostSrc/CMakeProject2/CMakeProject2.cpp
@@ -0,0 +1,23 @@
+﻿// CMakeProject2.cpp : Defines the entry point for the application.
+//
+#include <windows.h>
+#include <stdlib.h>
+#include <string.h>
+#include <tchar.h>
+#include "CMakeProject2.h"
+#include "MyPlugin.h"
+#include "MyPlugin2.h"
+
+using namespace std;
+
+int main()
+{
+	obs_module_load();
+	//obs_module_load_2();
+	cout << "Hello CMaksafasfgge." << endl;
+	MessageBox(NULL,
+		_T("Call to Create Window failed!"),
+		_T("Windows Desktop Guided Tour"),
+		NULL);
+	return 0;
+}
