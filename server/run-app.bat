b5cc5cc1139f0c44e0b3b43dddbb457f1276d103
giongto35 <thanhnguyen17101994@gmail.com>
2021-07-11 03:19:50 +0800

    update bat file

--- server/run-app.bat
+++ server/run-app.bat
@@ -1,3 +1,3 @@
+set /P PATH=PATH
 START /b notepad
-@echo off
 START /b ffmpeg -f gdigrab  -framerate 30 -i title="Untitled - Notepad" -pix_fmt yuv420p -vf scale=1280:-2 -c:v libvpx -f rtp rtp://127.0.0.2:5004
