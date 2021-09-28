7fb67cb63c3ea2a4c79cd17266af29f37b841a42
Anh Huynh <anh.huynh@shopee.com>
2021-09-28 21:08:13 +0800

    Refine look and feel

--- gui/renderer.js
+++ gui/renderer.js
@@ -1,25 +1,49 @@
 // include the ipc module to communicate with main process.
-const ipcRenderer = require('electron').ipcRenderer;
+const ipcRenderer = require("electron").ipcRenderer;
 
-const registerButton = document.getElementById('registerButton');
-const apppathText = document.getElementById('apppathText');
+const registerButton = document.getElementById("registerButton");
+const apppathText = document.getElementById("apppathText");
 
-registerButton.addEventListener('click', function () {
-    var arg = "secondparam";
+registerButton.addEventListener("click", function () {
+  var arg = "secondparam";
 
-    //send the info to main process . we can pass any arguments as second param.
-    // ipcRender.send will pass the information to main process. Here is event to open file dialog
-    ipcRenderer.send("register", arg);
+  //send the info to main process . we can pass any arguments as second param.
+  // ipcRender.send will pass the information to main process. Here is event to open file dialog
+  ipcRenderer.send("register", arg);
 });
 
-const connectWalletButton = document.getElementById('connectWalletButton');
-connectWalletButton.addEventListener('click', function() {
-   var arg = "secondparam";
-   
-   ipcRenderer.send("connectWallet", arg);
+const connectWalletButton = document.getElementById("connectWalletButton");
+connectWalletButton.addEventListener("click", function () {
+  var arg = "secondparam";
+
+  ipcRenderer.send("connectWallet", arg);
 });
 
-//ipcRenderer.on will receive the “btnclick-task-finished'” info from main process 
-ipcRenderer.on('registerFinished', function (event, param) {
-    console.log(param);
+//ipcRenderer.on will receive the “btnclick-task-finished'” info from main process
+ipcRenderer.on("registerFinished", function (event, param) {
+  console.log(param);
 });
+
+const setupModal = (modalId, btnId, spanId) => {
+  var modal = document.getElementById(modalId);
+  var btn = document.getElementById(btnId);
+  var span = document.getElementById(spanId);
+  console.log(modal);
+
+  btn.onclick = function () {
+    modal.style.display = "block";
+  };
+
+  span.onclick = function () {
+    modal.style.display = "none";
+  };
+
+  window.onclick = function (event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  };
+};
+
+setupModal("addAppModal", "addAppButton", "addAppClose");
+setupModal("connectWalletModal", "connectWalletButton", "connectWalletClose");
