7cb686f39735aa71aeea07bbe8ef09ab237851e2
lth08091998 <lth08091998@gmail.com>
2021-10-27 17:10:22 +0800

    Update

--- server/pkg/handler/base.go
+++ server/pkg/handler/base.go
@@ -18,7 +18,8 @@ func ApiHandlerWrapper(
 
 		// Reply preflight query to avoid CORS blocking
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
-		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+		//w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, OPTIONS, POST, DELETE")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With,Origin")
 		if r.Method == http.MethodOptions {
