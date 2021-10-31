260dd546bdb6f388b343a1fc472793c9cad7b824
lth08091998 <lth08091998@gmail.com>
2021-10-31 22:31:39 +0800

    Hotfix

--- server/init_db/init_db.sql
+++ server/init_db/init_db.sql
@@ -92,7 +92,3 @@ CREATE TABLE IF NOT EXISTS smart_otps (
     updated_at TIMESTAMP,
     deleted_at TIMESTAMP
 );
-
-INSERT INTO whitelisted_admins (id, wallet_address) VALUES (1, '0x3bfe0fcaecdb3ad87c786447f10d57bd0c6cb842');
-INSERT INTO whitelisted_admins (id, wallet_address) VALUES (3, '0xd4eece6df28f29eca940e52311f9f4d031e37a2d');
-INSERT INTO whitelisted_admins (id, wallet_address) VALUES (3, '0xe641a324ad3641658251b8d4a3c77b537134f6b0');
---
+++ server/init_db/prefill_data.sql
@@ -0,0 +1,5 @@
+INSERT INTO whitelisted_admins (id, wallet_address) VALUES (1, '0x3bfe0fcaecdb3ad87c786447f10d57bd0c6cb842');
+INSERT INTO whitelisted_admins (id, wallet_address) VALUES (3, '0xd4eece6df28f29eca940e52311f9f4d031e37a2d');
+INSERT INTO whitelisted_admins (id, wallet_address) VALUES (3, '0xe641a324ad3641658251b8d4a3c77b537134f6b0');
+
+INSERT INTO `users` (`id`,`created_at`,`updated_at`,`deleted_at`,`wallet_address`,`nonce`,`machine`,`location`,`name`,`status`) VALUES ("","2021-10-31 22:10:57.73","2021-10-31 22:10:57.73",NULL,"0x3bfe0fcaecdb3ad87c786447f10d57bd0c6cb842","NxvJbbNDGF","","","","0")
